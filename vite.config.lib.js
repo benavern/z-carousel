41d8daecd042269e5b86d1995b297e1942a2859f
Benjamin Caradeuc <bencrdc@gmail.com>
2024-11-27 14:47:41 +0100

    update deps + export types

--- vite.config.lib.js
+++ vite.config.lib.js
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vite';
+import dtsPlugin from 'vite-plugin-dts';
+
+const WebComponentFile = 'src/z-carousel.ts';
 
 export default defineConfig({
+    plugins: [
+        dtsPlugin({
+            insertTypesEntry: true,
+            include:  [WebComponentFile]
+        }),
+    ],
     build: {
         outDir: 'dist',
         lib: {
